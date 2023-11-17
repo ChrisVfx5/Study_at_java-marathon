fa51b3808aa27cc89abfe448b2567e3a41eb0a0c
Kristina Platonova <kplatonova39@gmail.com>
2023-11-17 22:07:57 +0300

    add day 4

---
+++ src/Day5/Task1.java
@@ -0,0 +1,31 @@
+package Day5;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+        car1.setInfo("Nissan", "Blue", 2015);
+        System.out.println("Информация о моделе:\n" + car1.getInfo());
+
+
+    }
+}
+
+class Car {
+    private String model;
+    private String color;
+    private int yearOfIssue;
+
+    protected void setInfo(String model, String color, int yearOfIssue){
+        this.model = model;
+        this.color = color;
+        this.yearOfIssue = yearOfIssue;
+    }
+
+    protected String getInfo(){
+        String info = "Модель: " + model + "\nЦвет: " + color + "\nГод выпуска: " + yearOfIssue;
+        return info;
+    }
+
+
+}
---
+++ src/Day5/Task2.java
@@ -0,0 +1,32 @@
+package Day5;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        Motorbike bike1 = new Motorbike("Hasegawa", "Green", 2020);
+        String getInfo = bike1.getInfo();
+        System.out.println("Motorbike information:\n" + getInfo);
+
+
+    }
+}
+
+class Motorbike{
+
+    private String model;
+    private String color;
+    private int  yearOfIssue;
+
+    protected Motorbike(String model, String color, int yearOfIssue){
+        this.model = model;
+        this.color = color;
+        this.yearOfIssue = yearOfIssue;
+    }
+
+    protected String getInfo(){
+        String info = "Model: " + model + "\nColor: " + color + "\nYear of issue: " + yearOfIssue;
+        return info;
+    }
+
+
+}
